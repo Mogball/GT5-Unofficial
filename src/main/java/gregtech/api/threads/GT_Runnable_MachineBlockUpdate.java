ef4ac82839c669d25e0e860ae9d1e66a20c50811
Mogball <jeffniu22@gmail.com>
2020-04-15 23:23:17 -0400

    remove max update depth

--- src/main/java/gregtech/api/threads/GT_Runnable_MachineBlockUpdate.java
+++ src/main/java/gregtech/api/threads/GT_Runnable_MachineBlockUpdate.java
@@ -20,9 +20,6 @@ public class GT_Runnable_MachineBlockUpdate implements Runnable {
     private final World mWorld;
     private final Set<ChunkPosition> mVisited;
 
-    // Hopefully large enough for most multi-block machines
-    private static final int MAX_UPDATE_DEPTH = 128;
-
     public GT_Runnable_MachineBlockUpdate(World aWorld, int aX, int aY, int aZ) {
         mWorld = aWorld;
         mX = aX;
@@ -49,7 +46,7 @@ public class GT_Runnable_MachineBlockUpdate implements Runnable {
     }
 
     private void stepToUpdateMachine(int aX, int aY, int aZ) {
-        if (!mVisited.add(new ChunkPosition(aX, aY, aZ)) || mVisited.size() > MAX_UPDATE_DEPTH)
+        if (!mVisited.add(new ChunkPosition(aX, aY, aZ)))
             return;
 
         TileEntity tTileEntity = mWorld.getTileEntity(aX, aY, aZ);
